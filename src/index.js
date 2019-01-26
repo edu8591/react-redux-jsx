8f76327ba49b9b44c076b8e1571917fd0beaecb3
Eduardo Serrano <eduardoserrano8591@gmail.com>
2019-01-26 01:22:03 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+//in order to show something in the react app
+
+//Step 1) import the react and react DOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//we have to import every library that we want to use in the prooject
+//diagram on how to use it:
+//import(we want to get code from some other file or dependency) React(the variable we want to assign this import to) from( we are aabout to specify the name of the library or file we are importing from) 'react'(the name of the dependency or path to the file we are importing)
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+//Step 2) Create a react component
+//a react component is a function or a class
+//that produces HTML to show to the user
+//and to handle feedback from the user(use event handlers)
+
+const App = () => { //arrow function instead of the function keyword
+    //classes and component always start with capital leters
+    return <div>Hi there!</div>;
+};
+//this is a function component
+
+
+//Step 3)Take the react component and show it on the screen
+
+ReactDOM.render(
+
+    <App />,
+    document.querySelector('#root') //thia is the second argument
+    //the second argument is a reference of a DOM element that exist in the html file
+);
+
+
+/**
+ * jsx is not html but a special dialect of js
+*/
